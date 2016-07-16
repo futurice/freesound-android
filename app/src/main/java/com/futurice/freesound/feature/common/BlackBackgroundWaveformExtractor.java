438934c14d91a01520e229dc089beb50d3d63402
Peter Tackage <peter.tackage@gmail.com>
2016-07-16 19:46:52 +0200

    Make implementation final

--- app/src/main/java/com/futurice/freesound/feature/common/BlackBackgroundWaveformExtractor.java
+++ app/src/main/java/com/futurice/freesound/feature/common/BlackBackgroundWaveformExtractor.java
@@ -4,7 +4,7 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.support.annotation.NonNull;
 
-public class BlackBackgroundWaveformExtractor extends WaveformExtractor {
+public final class BlackBackgroundWaveformExtractor extends WaveformExtractor {
 
     @Override
     public boolean isWaveform(@NonNull final Bitmap bitmap, final int x, final int y) {
