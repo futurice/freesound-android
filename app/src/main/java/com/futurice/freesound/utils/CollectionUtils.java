730427833469a73d2b281d9fdb6c91fe3424b34b
Lucia Payo <lucilu.p.m@gmail.com>
2016-08-07 11:07:55 +0200

    Allow null inputs and checks in CollectionUtils.areEqual

--- app/src/main/java/com/futurice/freesound/utils/CollectionUtils.java
+++ app/src/main/java/com/futurice/freesound/utils/CollectionUtils.java
@@ -1,6 +1,6 @@
 package com.futurice.freesound.utils;
 
-import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import java.util.Collection;
 import java.util.Iterator;
@@ -14,9 +14,9 @@ public final class CollectionUtils {
      * @param second Collection
      * @return True if the content and the order of the collection are equal, otherwise false
      */
-    public static <T> boolean areEqual(@NonNull final Collection<T> first,
-                                       @NonNull final Collection<T> second) {
-        if (first.size() != second.size()) {
+    public static <T> boolean areEqual(@Nullable final Collection<T> first,
+                                       @Nullable final Collection<T> second) {
+        if (first == null || second == null || first.size() != second.size()) {
             return false;
         }
 
