c6e947a20cc2f4a9f610ef5d0a9c6b8692a3e505
Marius Streilein <marius.streilein@futurice.com>
2016-11-11 14:11:50 +0100

    javaDoc for SearchSnackBar

--- app/src/main/java/com/futurice/freesound/feature/search/SearchSnackbar.java
+++ app/src/main/java/com/futurice/freesound/feature/search/SearchSnackbar.java
@@ -9,11 +9,21 @@ import polanski.option.Option;
 
 import static com.futurice.freesound.utils.Preconditions.checkNotNull;
 
+/**
+ * A helper class, which can display and dismiss a SnackBar
+ */
 class SearchSnackbar {
 
     @Nullable
     private Snackbar snackbar;
 
+    /**
+     * Shows a snackbar  indefinitely in the first parent view found above the provided view.
+     * Dismisses a previous SnackBar if one was shown before.
+     *
+     * @param view The view supposed to show the SnackBar. If not a CoordinatorLayout, SnackBar will find the first parent view which is suitable to hold a SnackBar
+     * @param charSequence The message to be displayed in the SnackBar
+     */
     void showNewSnackbar(@NonNull View view, @NonNull final CharSequence charSequence) {
         checkNotNull(view);
         checkNotNull(charSequence);
@@ -23,6 +33,9 @@ class SearchSnackbar {
         snackbar.show();
     }
 
+    /**
+     * Dismisses a previously shown SnackBar if it exists
+     */
     void dismissSnackbar() {
         Option.ofObj(snackbar)
               .ifSome(Snackbar::dismiss);
