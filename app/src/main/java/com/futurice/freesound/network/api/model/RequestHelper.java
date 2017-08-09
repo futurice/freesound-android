5afcacf5781c74e2cef25480e82f5ff9220f1735
Peter Tackage <peter.tackage@futurice.com>
2017-08-09 15:59:44 +0200

    Use joinToString extension rather than own implementation

--- app/src/main/java/com/futurice/freesound/network/api/model/RequestHelper.java
+++ app/src/main/java/com/futurice/freesound/network/api/model/RequestHelper.java
@@ -20,7 +20,7 @@ import com.futurice.freesound.common.InstantiationForbiddenError;
 
 import android.support.annotation.NonNull;
 
-import java.util.Iterator;
+import kotlin.collections.CollectionsKt;
 
 import static com.futurice.freesound.common.utils.Preconditions.checkNotNull;
 
@@ -29,17 +29,7 @@ final class RequestHelper {
     @NonNull
     static <T> String asCommaSeparated(@NonNull final Iterable<T> iterable) {
         checkNotNull(iterable);
-
-        StringBuilder sb = new StringBuilder();
-        Iterator<T> iter = iterable.iterator();
-        while (iter.hasNext()) {
-            String value = iter.next().toString();
-            sb.append(value);
-            if (iter.hasNext()) {
-                sb.append(',');
-            }
-        }
-        return sb.toString();
+        return CollectionsKt.joinToString(iterable, ",", "", "", -1, "", null);
     }
 
     private RequestHelper() {
