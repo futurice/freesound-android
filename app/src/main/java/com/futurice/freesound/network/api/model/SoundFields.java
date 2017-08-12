6b30d9da669f2322d4a51db9af6435b0835c0ed0
Peter Tackage <peter.tackage@gmail.com>
2017-08-12 13:43:03 +0200

    Use joinToString extension rather than own implementation (#148)

--- app/src/main/java/com/futurice/freesound/network/api/model/SoundFields.java
+++ app/src/main/java/com/futurice/freesound/network/api/model/SoundFields.java
@@ -64,7 +64,7 @@ public final class SoundFields {
 
     @Override
     public String toString() {
-        return RequestHelper.asCommaSeparated(fields);
+        return RequestsKt.asCommaSeparated(fields);
     }
 
     private static class Builder {
