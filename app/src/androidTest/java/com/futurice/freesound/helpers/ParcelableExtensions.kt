1b7249b57ac1895968a61b3d6d228d50cedbc25f
Tomek Polański <tomasz.polanski@futurice.com>
2018-02-05 18:09:09 +0100

    Testing Sound parcel (#172)

---
+++ app/src/androidTest/java/com/futurice/freesound/helpers/ParcelableExtensions.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2017 Futurice GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.futurice.freesound.helpers
+
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+
+inline fun <reified R : Parcelable> R.testParcel(): R =
+        marshallParcelable(this)
+                .let { unmarshallParcelable(it) }
+
+inline fun <reified R : Parcelable> marshallParcelable(parcelable: R): ByteArray =
+        Bundle().apply { putParcelable(R::class.java.name, parcelable) }
+                .let { marshall(it) }
+
+fun marshall(bundle: Bundle): ByteArray =
+        Parcel.obtain().use {
+            it.writeBundle(bundle)
+            it.marshall()
+        }
+
+inline fun <reified R : Parcelable> unmarshallParcelable(bytes: ByteArray): R = unmarshall(bytes)
+        .readBundle()
+        .run {
+            classLoader = R::class.java.classLoader
+            getParcelable(R::class.java.name)
+        }
+
+fun unmarshall(bytes: ByteArray): Parcel =
+        Parcel.obtain().apply {
+            unmarshall(bytes, 0, bytes.size)
+            setDataPosition(0)
+        }
+
+private fun <T> Parcel.use(block: (Parcel) -> T): T =
+        try {
+            block(this)
+        } finally {
+            this.recycle()
+        }
---
+++ app/src/androidTest/java/com/futurice/freesound/network/api/model/SoundParcelTest.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2017 Futurice GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.futurice.freesound.network.api.model
+
+import android.support.test.filters.SmallTest
+import android.support.test.runner.AndroidJUnit4
+import com.futurice.freesound.helpers.testParcel
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.util.*
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class SoundParcelTest {
+
+    @Test
+    fun sound_is_parcelable() {
+        val images = Image("medWav", "largeWav",
+                "medSpec", "largeSpec")
+        val preview = Preview("lowMp3", "highMp3",
+                "lowOgg", "highOgg")
+
+        val sound = Sound(
+                id = 123L,
+                url = "url",
+                name = "name",
+                tags = listOf("abc", "123"),
+                description = "desc",
+                geotag = GeoLocation(1.0, 2.0),
+                username = "username",
+                images = images,
+                previews = preview,
+                duration = 5.0F,
+                created = Date(1000)
+        )
+
+        sound.testParcel()
+                .apply {
+                    assertThat(this).isEqualTo(sound)
+                    assertThat(this).isNotSameAs(sound)
+                }
+    }
+
+}
